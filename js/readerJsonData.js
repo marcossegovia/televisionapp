ead5623ff284668d4c87ad94022b4834b45844e8
Marcos <velozmarkdrea@gmail.com>
2014-11-13 20:34:55 +0100

    json

---
+++ js/readerJsonData.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Marcos on 13/11/14.
+ */
+
+$.getJSON( "../assets/dataService/videos.json", function( data ) {
+
+    var videos = [];
+
+    $.each( data.videos, function( key, val ) {
+        videos.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+
+    $( "<ul/>", {
+        "class": "item-video",
+        html: items.join( "" )
+    }).appendTo( "body" );
+});
