b05f1c1033492b713ef51924d53822fdf10f988f
Marcos <velozmarkdrea@gmail.com>
2014-11-13 20:53:04 +0100

    json

--- js/readerJsonData.js
+++ js/readerJsonData.js
@@ -7,7 +7,12 @@ $.getJSON( "../assets/dataService/videos.json", function( data ) {
     var videos = [];
 
     $.each( data.videos, function( key, val ) {
-        videos.push( "<li id='" + key + "'>" + val + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.imgName + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.urlVideo + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.name + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.description + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.playbacks + "</li>" );
+        videos.push( "<li id='" + key + "'>" + val.author + "</li>" );
     });
 
     $( "<ul/>", {
