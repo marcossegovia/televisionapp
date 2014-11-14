7d143861e225a8a888304f3a80fe55c28c38dbae
Marcos <velozmarkdrea@gmail.com>
2014-11-14 14:41:39 +0100

    json

--- js/readerJsonData.js
+++ js/readerJsonData.js
@@ -8,12 +8,12 @@ $.getJSON( "../assets/dataService/videos.json", function( data ) {
     var numVideo=0;
 
     $.each( data.videos, function( key, val ) {
-        videos[numVideo]['imgName']=( "<li id='" + key + "'>" + val.imgName + "</li>" );
-        videos[numVideo]['urlVideo']=( "<li id='" + key + "'>" + val.urlVideo + "</li>" );
-        videos[numVideo]['name']=( "<li id='" + key + "'>" + val.name + "</li>" );
-        videos[numVideo]['description']=( "<li id='" + key + "'>" + val.description + "</li>" );
-        videos[numVideo]['playbacks']=( "<li id='" + key + "'>" + val.playbacks + "</li>" );
-        videos[numVideo]['author']=( "<li id='" + key + "'>" + val.author + "</li>" );
+        videos[numVideo]['imgName']= "<li id='" + key + "'>" + val.imgName + "</li>";
+        videos[numVideo]['urlVideo']= "<li id='" + key + "'>" + val.urlVideo + "</li>";
+        videos[numVideo]['name']= "<li id='" + key + "'>" + val.name + "</li>" ;
+        videos[numVideo]['description']= "<li id='" + key + "'>" + val.description + "</li>" ;
+        videos[numVideo]['playbacks']= "<li id='" + key + "'>" + val.playbacks + "</li>" ;
+        videos[numVideo]['author']= "<li id='" + key + "'>" + val.author + "</li>" ;
         numVideo++;
     });
 
