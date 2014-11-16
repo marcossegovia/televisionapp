5421920e6ea78fc197c6ee050654be0ece5ed3c9
Marcos <velozmarkdrea@gmail.com>
2014-11-16 15:22:49 +0100

    video

--- js/controllerButton.js
+++ js/controllerButton.js
@@ -2,12 +2,3 @@
  * Created by Marcos on 21/10/14.
  */
 
-function redButtonWelcoming()
-{
-    $('#button-message').delay(10000).fadeOut(1500);
-    $('#button-message').delay(5000).fadeIn(200);
-    $('#button-message').delay(5000).fadeOut(1500);
-    $('#button-message').delay(60000).fadeIn(200);
-    $('#button-message').delay(5000).fadeOut(1500);
-
-}
