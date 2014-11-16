ebd96f91532f6a37ccb656af139ed544b20567f5
Marcos <velozmarkdrea@gmail.com>
2014-11-16 14:47:11 +0100

    json

--- js/hbbtv.js
+++ js/hbbtv.js
@@ -110,12 +110,12 @@ function showVideo(typ, location) {
     // ignore
   }
   var mtype = typ ? 'video/mp4' : 'video/broadcast';
-  /*var ihtml = '<object id="video" type="'+mtype+'/object>';
+  var ihtml = '<object id="video" type="'+mtype+'" style="position: absolute; left:500px; top:100px; width:600px; height:450px;"><'+'/object>';
   elem.style.left = '0px';
   elem.style.top = '0px';
   elem.style.width = '1280px';
   elem.style.height = '720px';
-  elem.innerHTML = ihtml;*/
+  elem.innerHTML = ihtml;
   var succss = false;
   var phase = 1;
   try {
