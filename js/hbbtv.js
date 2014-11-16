305afc171ec97e30a8a347b0376163cf8af8b392
Marcos <velozmarkdrea@gmail.com>
2014-11-16 16:25:05 +0100

    video

--- js/hbbtv.js
+++ js/hbbtv.js
@@ -87,7 +87,7 @@ function showVideo(typ, page) {
     }
     if(page==3)
     {
-        var ihtml = '<object id="video" type="'+mtype+'" style=" width: 350px; height: 250px;""><'+'/object>';
+        var ihtml = '<object id="video" type="'+mtype+'" style=" position:absolute; width: 350px; height: 250px;""><'+'/object>';
 
     }
     elem.style.left = '0px';
